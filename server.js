6d6a0c0b8db380c348325b9db41fc95f396f562b
Charlotte Frates <charlottefrates@gmail.com>
2017-04-18 13:50:21 -0700

    Add files via upload
    recipe POST addition

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const bodyParser = require('body-parser');
 
 const {ShoppingList, Recipes} = require('./models');
 
+//anoter piece of middleware to create JSON objects
 const jsonParser = bodyParser.json();
+
+//runs express
 const app = express();
 
 // log the http layer
@@ -31,6 +34,7 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+//shopping list POST addition
 app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
@@ -42,9 +46,30 @@ app.post('/shopping-list', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-
+  // adds new posted items into main shopping list
   const item = ShoppingList.create(req.body.name, req.body.budget);
   res.status(201).json(item);
+});
+
+//recipe POST addition
+//not working on POSTMAN getting 404 error
+app.post('/recipes', jsonParser, (req, res) => {
+     //ensures that 'name' and 'ingredients' are in request body
+     const requiredFields = ['name','ingredients'];
+     // loop to check requiredFields
+     for (let i=0; i<requiredFields.length; i++) {
+       const field = requiredFields[i];
+            if (!(field in req.body)) {
+              const message = `Missing \`${field}\` in request body`
+              console.error(message);
+              return res.status(400).send(message);
+            }
+       };
+
+     // adds new recipe to main recipe list
+     const item = Recipes.create(req.body.name, req.body.ingredients);
+     res.status(201).json(item);
+
 });
 
 
